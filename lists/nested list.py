6de9a2e7e9db9ba784bfa90e3ffde82680db532f
bagewadirahulr-byte <bagewadirahulr@gmail.com>
2026-03-15 08:38:36 +0530

    Delete lists directory

--- lists/nested list.py
+++
@@ -1,7 +0,0 @@
-# nested list
-dirty_dozen =["mango", "pineapple", "orange", "onion", "tomato", "strawberry", "pear", "brinjal",
-              "beans", "ladyfinger", ]
-fruits = ["mango", "pineapple","orange", "strawberry", "pear",]
-vegetables = ["onion", "tomato", "brinjal", "beans", "ladyfinger"]
-dirty_dozen = [fruits, vegetables]
-print(dirty_dozen)
