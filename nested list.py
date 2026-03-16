5468f7060d9c6e5b44aac93057d609853a9f5700
bagewadirahulr-byte <bagewadirahulr@gmail.com>
2026-03-17 03:35:08 +0530

    Add files via upload

---
+++ nested list.py
@@ -0,0 +1,7 @@
+# nested list
+dirty_dozen =["mango", "pineapple", "orange", "onion", "tomato", "strawberry", "pear", "brinjal",
+              "beans", "ladyfinger", ]
+fruits = ["mango", "pineapple","orange", "strawberry", "pear",]
+vegetables = ["onion", "tomato", "brinjal", "beans", "ladyfinger"]
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen)
